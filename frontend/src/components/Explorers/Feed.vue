1e2604e506c87b6edd3ae03bfa9676b60a97b039
Oliver Lennartsson <oliverhenriklennartsson@gmail.com>
2020-08-20 14:11:48 +0200

    Added sticky buttons and created feed structure with testimg and placeholder for comments and likes  TBC

--- frontend/src/components/Explorers/Feed.vue
+++ frontend/src/components/Explorers/Feed.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="content">
       <div>FEED</div>
-      
+    <div class="image-container">
+        <img src="../../assets/logo.png" alt="">
+        <div class="commentlikes-container">comments likes
+            <button>likebutton</button>
+            <form action="">
+                <input type="text" placeholder="comment...">
+            </form>
+            <div class="comments">
+                <div class="comment-count">69 comments</div>
+                <div class="comment-container">
+                    <div class="user"></div>
+                    <div class="comment"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
   </div>
 </template>
 
--- frontend/src/views/ExplorerView.vue
+++ frontend/src/views/ExplorerView.vue
@@ -1,39 +1,59 @@
 <template>
   <div class="content">
-      <!-- Söka efter publika bilder -->
-      <Feed v-if="showFeed"/>
-      <Public v-else/>
+    <!-- Söka efter publika bilder -->
+    <Feed v-if="showFeed" />
+    <Public v-else />
 
-      <button @click="toggleFeed">FEED</button>
-      <button @click="toggleGrid">Masonry</button>
-      <!-- TEMP --> 
-      <!-- <router-link :to="{ name: 'AuthView', params: { page: 'login' }}"><span class="whiteColor">Login</span></router-link> -->
+    <div class="imagedisplay-buttons-container">
+      <span>
+        <button class="buttons feedbutton" @click="toggleFeed">FEED</button>
+        <button class="buttons" @click="toggleGrid">Masonry</button>
+      </span>
+    </div>
+    <!-- TEMP -->
+    <!-- <router-link :to="{ name: 'AuthView', params: { page: 'login' }}"><span class="whiteColor">Login</span></router-link> -->
   </div>
 </template>
 
 <script>
-import Feed from '../components/Explorers/Feed'
-import Public from '../components/Explorers/Public'
+import Feed from "../components/Explorers/Feed";
+import Public from "../components/Explorers/Public";
 
 export default {
-  name: 'ExplorerView',
-  components: {Feed, Public},
+  name: "ExplorerView",
+  components: { Feed, Public },
   data() {
     return {
-      showFeed: false
-    }
+      showFeed: false,
+    };
   },
   methods: {
     toggleFeed() {
-      this.showFeed = true
+      this.showFeed = true;
     },
     toggleGrid() {
-      this.showFeed = false
-    }
-  }
-}
+      this.showFeed = false;
+    },
+  },
+};
 </script>
 
 <style scoped>
+.imagedisplay-buttons-container {
+  width: 100%;
+  background-color: grey;
+  position: fixed;
+  bottom: 0;
+}
 
+/*  BUTTON ATTRIBUTES */  
+
+.buttons {
+  width: 40%;
+  height: 3em;
+}
+
+.feedbutton {
+ background-color: blue;
+}
 </style>
