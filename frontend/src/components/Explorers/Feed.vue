29298bb6b23065fdca74693c97971b5cba7d6eb8
Oliver Lennartsson <oliverhenriklennartsson@gmail.com>
2020-08-20 13:00:54 +0200

    Created feed.vue and buttons to toggle from masonry to feed in explorerview

---
+++ frontend/src/components/Explorers/Feed.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="content">
+      <div>FEED</div>
+      
+  </div>
+</template>
+
+<script>
+export default {
+  // created() {
+  //   fetch('http://localhost:8000/fileuploads/')
+  //     .then(response => response.json())
+  //     .then(result => {
+  //       this.images = result
+  //     })
+  // },
+  // data() {
+  //   return {
+  //     images: []
+  //   }
+  // },
+  name: 'Feed'
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/views/ExplorerView.vue
+++ frontend/src/views/ExplorerView.vue
@@ -1,15 +1,36 @@
 <template>
   <div class="content">
       <!-- Söka efter publika bilder -->
+      <Feed v-if="showFeed"/>
+      <Public v-else/>
 
+      <button @click="toggleFeed">FEED</button>
+      <button @click="toggleGrid">Masonry</button>
       <!-- TEMP --> 
-      <router-link :to="{ name: 'AuthView', params: { page: 'login' }}"><span class="whiteColor">Login</span></router-link>
+      <!-- <router-link :to="{ name: 'AuthView', params: { page: 'login' }}"><span class="whiteColor">Login</span></router-link> -->
   </div>
 </template>
 
 <script>
+import Feed from '../components/Explorers/Feed'
+import Public from '../components/Explorers/Public'
+
 export default {
-  name: 'ExplorerView'
+  name: 'ExplorerView',
+  components: {Feed, Public},
+  data() {
+    return {
+      showFeed: false
+    }
+  },
+  methods: {
+    toggleFeed() {
+      this.showFeed = true
+    },
+    toggleGrid() {
+      this.showFeed = false
+    }
+  }
 }
 </script>
 
