6f97eea80834d428e2811611815f351a06a85e00
Oliver Lennartsson <oliverhenriklennartsson@gmail.com>
2020-08-21 11:51:58 +0200

    added some functionality to feed(posts container)

--- frontend/src/components/Explorers/Feed.vue
+++ frontend/src/components/Explorers/Feed.vue
@@ -3,21 +3,23 @@
     <div>FEED</div>
     <div class="image-container">
       <img class="image" src="../../assets/logo.png" alt />
-      <div class="author">Frank666</div>
+      <div class="author">author: Frank666</div>
       <div class="commentlikes-container">
         <div class="likes">
           <span>69 spocks</span>
-          <button class="likebutton"> <font-awesome-icon :icon="['fas', 'hand-spock']" /></button>
+          <button class="likebutton button"> <font-awesome-icon :icon="['fas', 'hand-spock']" /></button>
         </div>
         <form action>
           <input type="text" placeholder="comment..." />
+          <button class="sendbutton button"><font-awesome-icon :icon="['fas', 'paper-plane']" /></button>
         </form>
 
         <div class="comments">
-          <div class="comment-count">view all 69 comments</div>
-          <div class="comment-container">
-            <div class="user">Frank</div>
+          <button @click="showComments" class="show-comment-button button">{{showCommentsbtnmsg}}</button>
+          <div v-if="commentsVisible" class="comment-container">
+            <div class="user">Frank:</div>
             <div class="comment">This is a pretty photo!</div>
+            <hr/>
           </div>
         </div>
       </div>
@@ -27,34 +29,42 @@
 
 <script>
 export default {
-  // created() {
-  //   fetch('http://localhost:8000/fileuploads/')
-  //     .then(response => response.json())
-  //     .then(result => {
-  //       this.images = result
-  //     })
-  // },
-  // data() {
-  //   return {
-  //     images: []
-  //   }
-  // },
+  data() {
+    return {
+      commentsVisible: false,
+      showCommentsbtnmsg: 'view all 69 comments'
+    }
+  },
+  methods: {
+    showComments() {
+      this.commentsVisible = !this.commentsVisible
+      if(this.commentsVisible) {
+        this.showCommentsbtnmsg = 'hide comments'
+      } else {
+        this.showCommentsbtnmsg = 'view all 69 comments' 
+      }
+    }
+  },
   name: "Feed",
 };
 </script>
 
 <style scoped>
+hr {
+  opacity: 0.1;
+}
+
 .image-container {
-  width: 100%;
-  max-width: 50em;
-  background-color: darkgrey;
+  width: 95%;
+  max-width: 30em;
+  background-color: teal;
   display: inline-block;
   font-family: Montserrat, sans-serif;
 }
 
 .image {
-  max-width: 20em;
-
+  width: 95%;
+  max-width: 30em;
 }
 
 .commentlikes-container {
@@ -67,15 +77,36 @@ export default {
   font-weight: 600;
 }
 
+.author {
+  text-align: start;
+  margin-left: 1em;
+}
+
 /** --------------BUTTONS----------- */
-.likebutton {
-  font-size: 2em;
+.button {
+  font-size: 1.5em;
   background-color: transparent;
   border-style: none;
   color: white;
+  cursor: pointer;
 }
 
 .likebutton:hover {
   color: orange;
+}
+
+.sendbutton:hover {
+  color: tomato;
+}
+
+.show-comment-button {
+  font-size: 1.3em;
+}
+
+.comment-container  {
+  text-align: left;
+  margin-left: 2em;
+  margin-right: 2em;
+  padding: 1em;
 }
 </style>
