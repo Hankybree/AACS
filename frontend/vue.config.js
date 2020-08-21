b532bdf6d7a8240db3a60bd5ca9c7bd0e30f938c
Jonathan Thunberg <54378565+Thunberg087@users.noreply.github.com>
2020-08-21 13:26:00 +0200

    Basic PWA functionality and changed name

---
+++ frontend/vue.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  pwa: {
+    name: 'PicNet',
+    themeColor: '#113260  ',
+    msTileColor: '#051D5A',
+    assetsVersion: 1.0,
+    manifestOptions: {
+      background_color: '#051D5A'
+    },
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    workboxOptions: {
+      runtimeCaching: [
+        {
+          handler: 'NetworkFirst',
+        }
+      ]
+    }
+  }
+};
