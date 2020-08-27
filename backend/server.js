12cebb9d1f45949695895058596ec16766732e4f
Thunberg <thunberg00@gmail.com>
2020-08-27 15:54:41 +0200

    Secure server and fixed websocket connection

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,27 @@ const bcrypt = require('bcryptjs')
 const cors = require('cors')
 const bodyParser = require("body-parser");
 const app = express()
+const http = require('http')
+const https = require('https')
+const fs = require('fs')
+const path = require('path')
+
 const port = 8000
 require('dotenv-flow').config();
 
-const server = app.listen(port, () => {
+if (process.env.NODE_ENV == 'production') {
+    console.log("Secure Server");
+    // Certificate
+    var privateKey = fs.readFileSync(path.resolve(__dirname, '/etc/letsencrypt/live/picnet.aviliax.com/privkey.pem'));
+    var certificate = fs.readFileSync(path.resolve(__dirname, '/etc/letsencrypt/live/picnet.aviliax.com/fullchain.pem'));
+    var sslcredentials = { key: privateKey, cert: certificate };
+    var server = https.createServer(sslcredentials, app);
+  } else {
+      console.log("Unsecure Server");
+    var server = http.createServer(app);
+  }
+
+server.listen(port, () => {
     console.log('Listening on port: ' + port)
 })
 
